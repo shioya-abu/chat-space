a0b8fdbaf898df404f67dd00ac0280177f72d272
shioya-abu <crapulence@yahoo.ne.jp>
2019-11-11 19:05:25 +0900

    flash and error messages, making and editing groups function

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,7 @@ class Group < ApplicationRecord
   
   has_many :groups_users
   has_many :users, through: :groups_users
+
   validates :name, presence: true, uniqueness: true
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,9 @@ class User < ApplicationRecord
   has_many :messages
   has_many :groups_users
   has_many :groups, through: :groups_users
+
+  validates :password, length: { minimum: 8, message: "は最低8文字入力してください。" }, format: { with: /\A[a-z\d]+\z/i, message: "は半角英数字のみ使用可能です。" }, on: :create
+  validates :email, format: { with: /\A[a-zA-Z0-9_#!$%&`'*+-{|}~^\/=?.]+@[a-zA-Z0-9][a-zA-Z0-9.-]+\z/ }
+  validates :name, presence: true, uniqueness: true
+
 end
