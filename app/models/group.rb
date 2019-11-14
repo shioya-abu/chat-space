d584c62a09edb51de668eb60745561cad37b3de5
shioya-abu <crapulence@yahoo.ne.jp>
2019-11-14 17:29:31 +0900

    edit messages html hamls and carrierwave and mini-magick

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,8 +1,15 @@
 class Group < ApplicationRecord
-  
+  has_many :messages
   has_many :groups_users
   has_many :users, through: :groups_users
 
   validates :name, presence: true, uniqueness: true
-  
+
+  def show_the_latest_message
+    if (latest_message = messages.last).present?
+      latest_message.content? ? latest_message.content : "*画像投稿あり"
+    else
+      "まだメッセージはありません。"
+    end    
+  end
 end
