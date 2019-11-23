2cbb3e52e9b9aa399ab81d0ceee2285e563dcbff
shioya-abu <crapulence@yahoo.ne.jp>
2019-11-23 18:47:40 +0900

    asynchronous HTML addition

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,44 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image) {
+      var html = `<div class="message">
+                    <div class="message__upper-info">
+                      <p class="message__upper-info__talker">
+                      ${message.user_name}
+                      </p>
+                      <p class="message__upper-info__date">
+                      ${message.created_at}
+                      </p>
+                    </div>
+                    <p class="message__text">
+                    ${message.content}
+                    </p>
+                    <div>
+                      <img src = '${message.image}'>
+                    </div>
+                  </div>`
+      return html;
+    } else {
+      var html = `<div class="message">
+                    <div class="message__upper-info">
+                      <p class="message__upper-info__talker">
+                      ${message.user_name}
+                      </p>
+                      <p class="message__upper-info__date">
+                      ${message.created_at}
+                      </p>
+                    </div>
+                    <p class="message__text">
+                    ${message.content}
+                    </p>
+                  </div>`
+      return html;
+    }
+  }
   $("#new_message").on('submit', function(e){
     e.preventDefault();
-    var formData = new FormData(this)
-    var url = $(this).attr('action')
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
     $.ajax({
       url: url,
       type: "POST",
@@ -10,6 +46,17 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $(".messages").append(html);
+      $("form")[0].reset();
+      $(".submit-btn").prop('disabled', false);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+    })
+    .fail(function(){
+      alert('メッセージ送信失敗!');
+      $(".submit-btn").prop('disabled', false);
     })
   })
 });
